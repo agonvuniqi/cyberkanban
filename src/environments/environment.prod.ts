06de3fa5e290819749e174eb41ff2cd5be0863d1
Agon <agonvuniqi@gmail.com>
2020-05-17 13:53:36 -0400

    Added login-page and user module

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBe2fs-8GWWL-6KO4QH8G58bEG1j7Mv-4g",
+    authDomain: "kanban-app-fb364.firebaseapp.com",
+    databaseURL: "https://kanban-app-fb364.firebaseio.com",
+    projectId: "kanban-app-fb364",
+    storageBucket: "kanban-app-fb364.appspot.com",
+    messagingSenderId: "755958413824",
+    appId: "1:755958413824:web:dd8fa73311ede5aa4eb6e5"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBe2fs-8GWWL-6KO4QH8G58bEG1j7Mv-4g",
+    authDomain: "kanban-app-fb364.firebaseapp.com",
+    databaseURL: "https://kanban-app-fb364.firebaseio.com",
+    projectId: "kanban-app-fb364",
+    storageBucket: "kanban-app-fb364.appspot.com",
+    messagingSenderId: "755958413824",
+    appId: "1:755958413824:web:dd8fa73311ede5aa4eb6e5"
+  }
 };
 
 /*
