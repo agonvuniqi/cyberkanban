49992b7ba236e1da5c9b4a1b6ec9d4e6c73d68cd
agonvuniqi <agonvuniqi@cvpcorp.com>
2020-06-10 23:44:26 -0400

    Added dynamic styling to the boards and created index to link firestore with the app

--- src/app/kanban/board-list/board-list.component.ts
+++ src/app/kanban/board-list/board-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Subscription } from 'rxjs';
+import { Board } from '../board.model';
+import { BoardService } from '../board.service'
 
 @Component({
   selector: 'app-board-list',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardListComponent implements OnInit {
 
-  constructor() { }
+  boards: Board[];
+  sub: Subscription;
 
-  ngOnInit(): void {
+  constructor(public boardService: BoardService) { }
+
+  ngOnInit() {
+    this.sub = this.boardService
+      .getUserBoards()
+      .subscribe(boards => (this.boards = boards));
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    // moveItemInArray = helper method that allows you to sort boards on the front end only
+    // Allows you to move tasks across different boards
+    moveItemInArray(this.boards, event.previousIndex, event.currentIndex);
+    this.boardService.sortBoards(this.boards);
   }
 
 }
--- src/app/kanban/board/board.component.ts
+++ src/app/kanban/board/board.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { moveItemInArray, CdkDragDrop } from '@angular/cdk/drag-drop';
+import { BoardService } from '../board.service';
+
 
 @Component({
   selector: 'app-board',
@@ -6,8 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board.component.scss']
 })
 export class BoardComponent implements OnInit {
+  @Input() board;
 
-  constructor() { }
+  constructor(private boardService: BoardService) { }
+
+  taskDrop(event: CdkDragDrop<string[]>) {
+    // Targets when the inner task object moves within the board
+    moveItemInArray(this.board.tasks, event.previousIndex, event.currentIndex);
+    this.boardService.updateTasks(this.board.id, this.board.tasks);
+  }
+
 
   ngOnInit(): void {
   }
