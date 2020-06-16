596982d4e48d4f977cfb90a0fb055ec7af6d4a6f
agonvuniqi <agonvuniqi@cvpcorp.com>
2020-06-16 00:52:18 -0400

    Added SEO service along with list/detail pages to display cards on the server

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,16 +5,24 @@ import { AuthGuard } from './user/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
+  //  Makes user module lazy loaded
   {
     path: 'login',
     loadChildren: () => import('./user/user.module').then((m) => m.UserModule),
   },
+  //  Makes kanban module lazy loaded
   { 
     path: 'kanban', 
     loadChildren: () => 
       import('./kanban/kanban.module').then(m => m.KanbanModule),
     canActivate: [AuthGuard]
  },
+//  Makes customers module lazy loaded
+ {
+   path: 'customers',
+   loadChildren: () =>
+    import('./customers/customers.module').then(m => m.CustomersModule),
+ }
 ];
 
 @NgModule({
